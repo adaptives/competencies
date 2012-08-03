46c227be4caa7fc0ea132804a516ec985ec37758
Parag Shah <adaptives@gmail.com>
2012-08-03 16:30:46 +0530

    Added model and controller for key value data

---
+++ app/controllers/KeyValueDatas.java
@@ -0,0 +1,5 @@
+package controllers;
+
+public class KeyValueDatas extends CRUD {
+
+}
---
+++ app/models/KeyValueData.java
@@ -0,0 +1,37 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+
+import play.db.jpa.Model;
+
+@Entity
+public class KeyValueData extends Model {
+	public String k;
+	@Lob
+	public String v;
+	
+	public KeyValueData(String key, String value) {
+		this.k = key;
+		this.v = value;
+		create();
+	}
+	
+	public static String findValue(String key) {
+		return findValue(key, null);
+	}
+	
+	public static String findValue(String key, String defaultVal) {
+		String retVal = defaultVal;
+		KeyValueData keyValueData = KeyValueData.find("select kvd from KeyValueData kvd where kvd.k = ?", key).first();
+		if(keyValueData != null) {
+			retVal = keyValueData.v;
+		}
+		return retVal;
+	}
+	
+	@Override
+	public String toString() {
+		return k;
+	}
+}
