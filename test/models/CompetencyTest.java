7e69061ae87d1b7d70f175b588d5acae19b9c9de
Kalpak Gadre <kalpakrg@gmail.com>
2012-08-06 16:07:06 +0530

    Added test for testing relationships.

--- test/models/CompetencyTest.java
+++ test/models/CompetencyTest.java
@@ -52,4 +52,21 @@ public final class CompetencyTest extends UnitTest {
             // Expected.
         }
     }
+
+    @Test
+    public void testCompetencyRelationships() {
+        Competency competency1 = new Competency("Competency 1", "Competency 1 description", group, level, "Competency 1 resources");
+        competency1.save();
+
+        Competency competency2 = new Competency("Competency 2", "Competency 2 description", group, level, "Competency 2 resources");
+        competency2.prerequisites.add(competency1);
+        competency2.save();
+
+        Competency found = Competency.findByTitle("Competency 2");
+        Assert.assertNotNull(found);
+        Assert.assertEquals("Competency 2",found.title);
+        Assert.assertNotNull(competency2.prerequisites);
+        Assert.assertEquals(1, competency2.prerequisites.size());
+        Assert.assertEquals("Competency 1", competency2.prerequisites.iterator().next().title);
+    }
 }
